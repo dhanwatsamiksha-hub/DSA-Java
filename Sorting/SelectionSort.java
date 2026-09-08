c5b87d420ab778e829b06171aeb5c0dac44145e4
Samiksha Dhanwat <dhanwatsamiksha@gmail.com>
2026-09-08 16:54:48 +0530

    Add SelectionSort implementation in Java
    Implement Selection Sort algorithm to sort an array of integers input by the user.

---
+++ Sorting/SelectionSort.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < n - 1; i++) {
+
+            int min = i;
+
+            for (int j = i + 1; j < n; j++) {
+
+                if (arr[j] < arr[min]) {
+                    min = j;
+                }
+            }
+
+            // Swap
+            int temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+        }
+
+        System.out.println("Sorted array:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        sc.close();
+    }
+}
